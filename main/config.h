e4b3ac36329aa47585e36892e279dfd8f0c7966c
Jose Ramon Navarro Marquez <8094349+Ardestino@users.noreply.github.com>
2025-08-12 17:42:38 -0600

    added default configurations

--- main/config.h
+++ main/config.h
@@ -5,8 +5,8 @@
 
 // Configuración WiFi
 // Modifica estos valores con tus credenciales de red WiFi
-#define WIFI_SSID "Totalplay-BBAB"
-#define WIFI_PASS "BBAB6E45BGkpHq9G"
+#define WIFI_SSID "Cabeza"
+#define WIFI_PASS "seguridad"
 
 // Configuración avanzada WiFi
 #define WIFI_MAXIMUM_RETRY 10
